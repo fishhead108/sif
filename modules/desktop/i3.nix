04ccf19babe96f2b91bb3a4eab425a8862db9f19
Dmitriy Miroshnichenko <2543810@gmail.com>
2022-02-22 18:11:58 +0100

    test

--- modules/desktop/i3.nix
+++ modules/desktop/i3.nix
@@ -7,7 +7,10 @@ let
 in {
 
   config = mkIf desktopMode {
-    services.xserver.windowManager.i3 = {
+    sys.users.allUsers.home.file.".config/i3/rofi_powermenu.sh".source = ./i3/rofi_powermenu.sh;
+    sys.users.allUsers.home.file.".config/i3/rofi_custom.sh".source = ./i3/rofi_custom.sh;
+    sys.users.allUsers.home.file.".config/i3/polybar.sh".source = ./polybar/launch.sh;
+    sys.users.allUsers.xsession.windowManager.i3 = {
       enable = true;
       package = pkgs.i3-gaps;
       extraConfig = "popup_during_fullscreen smart\ndefault_border pixel 1\ndefault_floating_border pixel 1\n";
@@ -183,39 +186,4 @@ in {
         ];
       };
     };
-
-    sys.users.allUsers.files = {
-      i3blocksconfig = {
-        path = ".config/i3/i3blocks.conf";
-        text = ''
-          [cpu_usage]
-          command=echo " $(grep 'cpu ' /proc/stat | awk '{usage=int(($2+$4)*100/($2+$4+$5))} END {print usage "%"}')"
-          interval=5
-
-          [memory_usage]
-          command=grep -E 'MemTotal|MemFree' /proc/meminfo | awk '/MemTotal/ {total=int($2/1024)} /MemFree/ {free=int($2/1024)} {used=total-free} END {print " " used "/" total "MB"}'
-          interval=5
-
-          [diskspace]
-          command=echo " $(df / -H | grep '/' | awk '{print $3 "/" $2}')"
-          interval=60
-
-          # Update time every 5 seconds
-          [time]
-          command=date "+%Y-%m-%d %T"
-          interval=5 
-        '';
-      };
-
-      i3config = {
-        path = ".config/i3/config";
-        text = let 
-          binDir = "/run/current-system/sw/bin";
-        in ''
-          ${config.sys.desktop.tilewm.extraConfig}
-          ${config.sys.desktop.tilewm.extraConfigi3}
-        '';
-      };
-    };
-  };
 }
